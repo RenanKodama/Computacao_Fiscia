175e0528cfc7a307297bd996e721483420ae6e26
RENAN KODAMA RODRIGUES <a1602098@debian9-dacom.dacom>
2018-04-03 17:27:47 -0300

    sensor de temperatura

--- Sensor_de_Saude/Sensor_de_Saude.ino
+++ Sensor_de_Saude/Sensor_de_Saude.ino
@@ -10,7 +10,9 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 PulseSensorPlayground pulseSensor;
 const int Pulse = 4;    //leitor BPM
 const int Led13 = 13;   //led
-int Signal;            //sinal
+const int Temp = 3;     //leitor de Temperatura
+float Temperatura;
+int Signal;             //sinal
 int Hold = 550;
 
 
@@ -30,6 +32,8 @@ void setup() {
     Serial.println("Ativando Leitor de Batimentos Cardiacos!"); 
     //lcd.print("Ativando Leitor de Batimentos Cardiacos!");
   }
+
+  
 }
 
 
@@ -42,16 +46,26 @@ void loop() {
   Signal = analogRead(Pulse);
   
   //Serial.print("Sinal: ");
-  //Serial.println(Signal);
+  //Serial.println(Signal); 
+
+  
+  Temperatura = ( 5.0 * analogRead(Temp) * 100.0) / 1024.0;   
+  //Serial.print("Temperatura: ");
+  //Serial.println(Temperatura);
+  lcd.setCursor(0,0);
+  lcd.print(Temperatura);
+  
 
   if (pulseSensor.sawStartOfBeat()) {           
    Serial.println("Lendo Batimentos Cardiacos! ♥"); 
    Serial.print("BPM: ");                      
-   Serial.println(myBPM);                      
+   Serial.println(myBPM);                    
 
+   lcd.setCursor(0,1);
    
    lcd.print("BPM: ");                      
    lcd.print(myBPM);  
+   //lcd.print(" ♥");
   }
 
   delay(20);
